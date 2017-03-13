5a42fa0f1b4746198b3890b91ae62b86052d88a1
袁小荣 <664995775@qq.com>
2017-03-13 17:08:59 +0800

    封装自己的方法

--- 04 前端基本功/常用案例/Scroll家族/my.js
+++ 04 前端基本功/常用案例/Scroll家族/my.js
@@ -1,24 +1,46 @@
+// $符号获取id
 function $(id) { return document.getElementById(id);}
-
+// 元素显示
 function show(obj) { obj.style.display = "block";}
-
+// 元素隐藏
 function hide(obj) { obj.style.display = "none";}
 
+// 兼容获取scroll的top,left值,使用方法: scroll.top, scroll.left
 function scroll() {
-    if(window.pageXOffset != null) { // w3c的浏览器
+    if(window.pageXOffset != null) { // ie9 +  最新浏览器
         return {
             top: window.pageYOffset,
             left: window.pageXOffset
         }
-    } else if(document.compatMode == "CSS1Compat") { // commonMode模式的
+    } else if(document.compatMode == "CSS1Compat") { // 标准浏览器
         return {
             top: document.documentElement.scrollTop,
             left: document.documentElement.scrollLeft
         }
-    } else { // 其它
+    } else { // 怪异浏览器
         return {
             top: document.body.scrollTop,
             left: document.body.scrollLeft
+        }
+    }
+}
+
+// 兼容获取client的width,height值,使用方法: client.width, client.height
+function client() {
+    if(window.innerWidth != null || window.innerHeight != null) { // ie9 +  最新浏览器
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    } else if(document.compatMode === "CSS1Compat") { // 标准浏览器
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }
+    } else {
+        return {   // 怪异浏览器
+            width: document.body.clientWidth,
+            height: document.body.clientHeight
         }
     }
 }
